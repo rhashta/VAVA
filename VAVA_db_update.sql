a12265b2ec6161957e2febd5f29b5bd0d9cdaf3f
Michal Krajčovič <09krajcovic@gmail.com>
2017-05-10 23:37:14 +0200

    scripty na db

---
+++ VAVA_db_update.sql
@@ -0,0 +1,14 @@
+-- timestamp 7 --
+insert into student_excercise (idstudent, idexcercise) values (5,4);
+-- timestamp 33 --
+insert into student_excercise (idstudent, idexcercise) values (5,17);
+-- timestamp 55 --
+insert into student_excercise (idstudent, idexcercise) values (5,42);
+-- timestamp 58 --
+insert into student_excercise (idstudent, idexcercise) values (5,80);
+-- timestamp 80 --
+insert into student_excercise (idstudent, idexcercise) values (5,97);
+-- timestamp 60 --
+insert into student_excercise (idstudent, idexcercise) values (5,113);
+-- timestamp 62 --
+insert into student_excercise (idstudent, idexcercise) values (5,155);
